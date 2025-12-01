5c50487ff7d9fb61e60ab79e23a83164285fc996
chickenwings <cysc.vizk@gmail.com>
2025-12-01 17:35:18 +0100

    Implement user CRUD SQL functions
    Added SQL functions for user CRUD operations including create, read, update, delete, and list.

--- src/server/db_manager/user.py
+++ src/server/db_manager/user.py
@@ -1,3 +1,45 @@
 # functions to create sql queries for all crud tasks
 
 # user create function sql statement creation
+
+# -------------------- CREATE --------------------
+def sql_create_user():
+    return """
+        INSERT INTO user_table 
+        (username, first_name, last_name, email, password)
+        VALUES (?, ?, ?, ?, ?)
+    """
+
+
+# -------------------- READ --------------------
+def sql_get_user_by_id():
+    return "SELECT * FROM user_table WHERE id = ?"
+
+
+def sql_get_user_by_username():
+    return "SELECT * FROM user_table WHERE username = ?"
+
+
+# -------------------- UPDATE --------------------
+def sql_update_user():
+    return """
+        UPDATE user_table
+        SET 
+            username = ?, 
+            first_name = ?, 
+            last_name = ?, 
+            email = ?, 
+            password = ?, 
+            updated_at = CURRENT_TIMESTAMP
+        WHERE id = ?
+    """
+
+
+# -------------------- DELETE --------------------
+def sql_delete_user():
+    return "DELETE FROM user_table WHERE id = ?"
+
+
+# -------------------- LIST --------------------
+def sql_list_users():
+    return "SELECT * FROM user_table ORDER BY created_at DESC"
